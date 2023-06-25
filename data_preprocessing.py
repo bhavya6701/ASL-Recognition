d3fcc0fc1c7748d0d447281dfa41e650bd9f3dba
bhavya <bhavya6701@users.noreply.github.com>
2023-06-24 23:05:09 -0400

    Data preprocessing updated and need to fix model.fit error

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -16,13 +16,14 @@ def process_data():
     data = []
     labels = []
 
-    for directory in os.listdir(DATA_DIR)[:1]:
+    for directory in os.listdir(DATA_DIR):
         path = os.path.join(DATA_DIR, directory)
         if not os.path.isdir(path):
             continue
 
+        count = 0
         for img in os.listdir(path):
-            data_aux = []
+            normalized_data = []
             img_path = os.path.join(path, img)
             img_rgb = cv2.cvtColor(cv2.imread(img_path), cv2.COLOR_BGR2RGB)
             landmarks = hands.process(img_rgb)
@@ -42,16 +43,23 @@ def process_data():
                     for i in range(len(hand.landmark)):
                         x = hand.landmark[i].x
                         y = hand.landmark[i].y
-                        data_aux.append(x - min(x_points))
-                        data_aux.append(y - min(y_points))
+                        normalized_data.append(x - min(x_points))
+                        normalized_data.append(y - min(y_points))
 
-                data.append(data_aux)
+                data.append(normalized_data)
                 labels.append(directory)
+
+            count += 1
+            print(count)
 
     f = open('data.pickle', 'wb')
     pickle.dump({'data': data, 'labels': labels}, f)
     f.close()
     print('Data saved')
 
+def main():
+    process_data()
 
-process_data()
+
+if __name__ == '__main__':
+    main()
--- ml_training.py
+++ ml_training.py
@@ -1,29 +1,32 @@
 import keras
+import pickle
+
+import numpy as np
 from matplotlib import pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
 
 def create_model():
     model = keras.Sequential(
         [
-            keras.Input(shape=(28, 28, 1)),
-            keras.layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
-            keras.layers.MaxPooling2D(pool_size=(2, 2)),
-            keras.layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
-            keras.layers.MaxPooling2D(pool_size=(2, 2)),
-            keras.layers.Flatten(),
-            keras.layers.Dropout(0.5),
-            keras.layers.Dense(47, activation="softmax"),
+            keras.layers.Dense(128, input_shape=(42,), activation="relu"),
+            keras.layers.Dense(64, activation="relu"),
+            keras.layers.Dense(32, activation="relu"),
+            keras.layers.Dense(16, activation="relu"),
+            keras.layers.Dense(1, activation="softmax"),
         ]
     )
-
     model.summary()
     return model
 
 
-def train_model(model, data, batch_size=128, epochs=15):
-    model.compile(loss="categorical_crossentropy", optimizer="SGD", metrics=["accuracy"])
-    training_history = model.fit(data.x_train, data.y_train, batch_size=batch_size, epochs=epochs, validation_split=0.2)
-    score = model.evaluate(data.x_test, data.y_test, verbose=0)
+def train_model(model, data, labels, batch_size=128, epochs=15):
+    x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
+
+    model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+    training_history = model.fit(x_train, y_train, epochs=epochs, validation_split=0.25)
+    score = model.evaluate(x_test, y_test, verbose=0)
     print("Test loss:", score[0])
     print("Test accuracy:", score[1])
     return training_history, model
@@ -38,10 +41,15 @@ def plot_learning_curve(training):
     plt.legend(['train', 'test'], loc='upper left')
     plt.show()
 
+
 def main():
+    data_labels = pickle.load(open('data.pickle', 'rb'))
+    filtered_data = np.asarray([data for data, labels in zip(data_labels['data'], data_labels['labels']) if len(data) == 42])
+    filtered_labels = np.asarray([labels for data, labels in zip(data_labels['data'], data_labels['labels']) if len(data) == 42])
+
     model = create_model()
-    # training = train_model(model, data, 128, 50)
-    # plot_learning_curve(training)
+    training = train_model(model, filtered_data, filtered_labels, 128, 5)
+    plot_learning_curve(training)
 
     # Save the trained model
     model.save('emnist_merge_recognition_model')
